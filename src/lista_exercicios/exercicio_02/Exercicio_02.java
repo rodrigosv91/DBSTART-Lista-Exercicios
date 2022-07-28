c52f0d186228e31ff0abf1e2c11a4a317e7f4c82
rodrigo.vieira <rodrigo.vieira@dbserver.com.br>
2022-07-28 13:33:17 -0300

    edit: exercicio 02

--- src/lista_exercicios/exercicio_02/Exercicio_02.java
+++ src/lista_exercicios/exercicio_02/Exercicio_02.java
@@ -17,29 +17,39 @@ public class Exercicio_02 {
             d. Acima de 50 unidades: 25% de desconto
         */
         
+        final int VALOR_DESCONTO_BASE = 0;
+        
+        final int QUANTIDADE_PRODUTO_PARA_DESCONTO_MINIMO = 10;
+        final int QUANTIDADE_PRODUTO_PARA_DESCONTO_MEDIO = 21;
+        final int QUANTIDADE_PRODUTO_PARA_DESCONTO_MAXIMO = 51;
+        
+        final double DESCONTO_MINIMO = 0.10;
+        final double DESCONTO_MEDIO = 0.20;
+        final double DESCONTO_MAXIMO = 0.25;
+         
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Informe o nome do produto:");    
-        String nome = sc.next();
+        String nome_produto = sc.next();
         
         System.out.println("Informe o preco do produto");
-        double preco = sc.nextDouble();
+        double preco_produto = sc.nextDouble();
         
         System.out.println("Informe a quantidade");
-        int quantidade = sc.nextInt();
+        int quantidade_produto = sc.nextInt();
         
-        double valorBase = quantidade*preco;
-        double desconto = 0;
+        double valor_produto_base = quantidade_produto*preco_produto;
+        double desconto_produto = VALOR_DESCONTO_BASE;
         
-        if(quantidade > 10 && quantidade <= 20)
-                desconto = valorBase * 0.10 ;       
-        if(quantidade > 20 && quantidade <= 50)
-                desconto = valorBase * 0.2;      
-        if(quantidade > 50)
-                desconto = valorBase * 0.25;
+        if(quantidade_produto > QUANTIDADE_PRODUTO_PARA_DESCONTO_MINIMO && quantidade_produto < QUANTIDADE_PRODUTO_PARA_DESCONTO_MEDIO)
+                desconto_produto = valor_produto_base * DESCONTO_MINIMO ;       
+        if(quantidade_produto >= QUANTIDADE_PRODUTO_PARA_DESCONTO_MEDIO && quantidade_produto < QUANTIDADE_PRODUTO_PARA_DESCONTO_MAXIMO)
+                desconto_produto = valor_produto_base * DESCONTO_MEDIO;      
+        if(quantidade_produto >= QUANTIDADE_PRODUTO_PARA_DESCONTO_MAXIMO)
+                desconto_produto = valor_produto_base * DESCONTO_MAXIMO;
         
-        System.out.println("Produto: " + nome);
-        System.out.println("Valor Total: " + ( valorBase - desconto));
+        System.out.println("Produto: " + nome_produto);
+        System.out.println("Valor Total: " + ( valor_produto_base - desconto_produto));
         
         sc.close();            
     }   
