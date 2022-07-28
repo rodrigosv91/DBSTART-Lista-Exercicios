08ddc3037cde492c5dd766065a7b2b8fc32d490a
rodrigo.vieira <rodrigo.vieira@dbserver.com.br>
2022-07-28 09:11:26 -0300

    edit: exercicio 18

--- src/lista_exercicios/exercicio_18/Exercicio_18.java
+++ src/lista_exercicios/exercicio_18/Exercicio_18.java
@@ -3,6 +3,16 @@ package lista_exercicios.exercicio_18;
 public class Exercicio_18 {
 
     public static void main(String[] args) {
+        /*
+        18) Faça uma classe Automóvel, identificando seus atributos e 
+            métodos. Herdando tais características, crie três classes: Carro, 
+            Moto e Guincho. Herdando Carro, faça uma classe 
+            CarroAutomático, na qual, para poder ligar, o freio precisa estar 
+            sendo pressionado. Herdando Moto, faça uma classe 
+            MotoPartidaPedal, onde, para ligar, o acelerador precisa estar 
+            puxado. O guincho pode carregar tanto carro quanto moto. Utilize 
+            os 4 pilares da POO.
+        */
        
         //Carro Automatico
         CarroAutomatico carroAut = new CarroAutomatico(4, 300);
