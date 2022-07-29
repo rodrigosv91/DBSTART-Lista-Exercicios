8488bc4c40fb80c735fa347917cc8fec2c8e7582
rodrigo.vieira <rodrigo.vieira@dbserver.com.br>
2022-07-29 11:06:45 -0300

    edit: exercicio 15

--- src/lista_exercicios/exercicio_15/Exercicio_15_old.java
+++ src/lista_exercicios/exercicio_15/Exercicio_15_old.java
@@ -2,41 +2,46 @@ package lista_exercicios.exercicio_15;
 
 import java.util.Scanner;
 
-public class Exercicio_15 {
+public class Exercicio_15_old {
 
     public static void main(String[] args) {
+        
         /*
         15) Crie uma classe Calculadora que faça as operações de soma, 
             subtração, multiplicação, divisão e potenciação. Cada operação 
             deve ser um método e todos os métodos devem ser estáticos.
             Para fazer a escolha das operações use como base o exercício 4
-        */        
-        Scanner sc = new Scanner(System.in);               
+        */
+        
+        Scanner sc = new Scanner(System.in);
+                
         System.out.println("Calculadora");
         System.out.println("OPCOES: 1 SOMAR | 2 SUBTRAIR | 3 MULTIPLICAR | 4 DIVIDIR");
         System.out.println("\nDigite a operacao que deseja executar:");
+    
         int opcao = sc.nextInt();
+        
         System.out.println("Digite o primeiro valor:");
-        double primeiro_numero = sc.nextDouble();
+        double valor1 = sc.nextDouble();
         System.out.println("Digite o segundo valor:");
-        double segundo_numero = sc.nextDouble();
+        double valor2 = sc.nextDouble();
                  
         switch (opcao){
             case 1: 
-                Calculadora.somar(primeiro_numero, segundo_numero);
+                Calculadora.somar(valor1, valor2);
                 break;
             case 2:
-                Calculadora.subtrair(primeiro_numero, segundo_numero);
+                Calculadora.subtrair(valor1, valor2);
                 break;
             case 3:
-                Calculadora.multiplicar(primeiro_numero, segundo_numero);
+                Calculadora.multiplicar(valor1, valor2);
                 break;
             case 4:
-                Calculadora.dividir(primeiro_numero, segundo_numero);
+                Calculadora.dividir(valor1, valor2);
                 break;
             default:
-                System.out.println("Opcão invalida");
+                System.out.println("Opcao invalida");
         }        
-        sc.close();      
-    } 
+        sc.close();
+    }   
 }
